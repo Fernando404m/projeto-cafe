e1bb7d7c31173712f437e138e0ceb32e59ee83d5
Fernando404m <159110705+Fernando404m@users.noreply.github.com>
2024-09-22 18:18:48 -0300

    " iniciando menu "

---
+++ cafe/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBatO1ZzVGXFbMw2oTd4Rz1Dg34XMHhR2M",
+  authDomain: "sabor-aroma.firebaseapp.com",
+  projectId: "sabor-aroma",
+  storageBucket: "sabor-aroma.appspot.com",
+  messagingSenderId: "1046425333570",
+  appId: "1:1046425333570:web:a2ba852269f3a7b15022db"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
